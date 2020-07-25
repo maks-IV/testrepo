8aceebbf03b837fb26e6aeee745d9cb64985c042
Maksym Ivasenko <ivasenko182@gmail.com>
2020-07-25 20:13:02 +0300

    Add project files.

---
+++ GitTrain/Classes/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTrain.Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public int Name { get; set; }
+    }
+}
---
+++ GitTrain/Program.cs
@@ -0,0 +1,14 @@
+﻿using GitTrain.Classes;
+using System;
+
+namespace GitTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
