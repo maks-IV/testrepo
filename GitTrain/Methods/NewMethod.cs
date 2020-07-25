6bd7a652f6777ca753eda1c605cfaecb334691eb
Maksym Ivasenko <ivasenko182@gmail.com>
2020-07-25 20:18:37 +0300

    changes master

---
+++ GitTrain/Methods/NewMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTrain.Methods
+{
+    class NewMethod
+    {
+        public NewMethod()
+        {
+
+        }
+    }
+}
--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -8,7 +8,13 @@ namespace GitTrain
         static void Main(string[] args)
         {
             Car car = new Car();
+            Car car3 = new Car();
             Console.WriteLine("Hello World!");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("New Method2!");
         }
     }
 }
