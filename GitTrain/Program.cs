167a51c29982dc9717b3c4ec3629e7d76a66a70e
Maksym Ivasenko <ivasenko182@gmail.com>
2020-07-26 13:16:44 +0300

    hjh

--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -11,6 +11,9 @@ namespace GitTrain
             Car car2 = new Car();
             Car car3 = new Car();
             Car car4 = new Car();
+            Car car8 = new Car();
+            Car car9 = new Car();
+            Car car10 = new Car();
             car.Name = "Audi";
             Console.WriteLine("Hello World!");
         }
